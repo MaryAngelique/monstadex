30f1a5b745d64c4369a8abce96a58de9823aae52
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-22 16:17:12 -0400

    Create React JS App: Monstadex

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "Angelique"
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Hi {this.state.name}</p>
+          <button>Change Name</button>
+        </header>
+      </div>
+    );
+  };
+}
+
+export default App;
