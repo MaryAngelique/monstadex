6ef1a51c4736994938ab40f8bc235600c1d10ff6
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-23 10:59:01 -0400

    Implemented monster names

--- src/App.js
+++ src/App.js
@@ -7,25 +7,30 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: "Angelique", lastName: "Abacajan"},
-      company: "ZMT"
+      monsters: [
+        {
+          name: "Linda"
+        },
+  
+        {
+          name: "Frank"
+        },
+  
+        {
+          name: "Jacky"
+        },
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name}, I work at {this.state.company}</p>
-          <button 
-            onClick={() => {
-              this.setState({ name: "Mary" });
-              console.log(this.state);
-          }}>
-            Change Name
-          </button>
-        </header>
+        {
+          this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>
+          })
+        }
       </div>
     );
   };
