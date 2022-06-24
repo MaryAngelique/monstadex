a11cd0f3e24f91448d6f39f5a8e183680e873217
Angelique Abacajan <ma_utsey@yahoo.com>
2022-06-24 12:51:58 -0400

    Implemented card component and styling

--- src/components/cards-list/card-list.component.jsx
+++ src/components/cards-list/card-list.component.jsx
@@ -1,22 +1,29 @@
 import { Component } from "react";
 
+import "./card-list.styles.css"
+import Card from "../card/card.component";
+
 class CardList extends Component {
 
     render() {
         const { monsters } = this.props;
 
         return(
-            <div>
+            <div className="card-list">
                 { monsters.map ((monster) => {
-                    <div className="card-container" key={monster.id}>
-                        <img 
-                            alt={`monster ${monster.name}`} 
-                            src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
-                        />
+                    const { name, email, id } = monster;
 
-                        <h2>{monster.name}</h2>
-                        <p>{monster.email}</p>
-                    </div>
+                    return (
+                        <div className="card-container" key={monster.id}>
+                            <img 
+                                alt={`monster ${monster.name}`} 
+                                src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
+                            />
+
+                            <h2>{monster.name}</h2>
+                            <p>{monster.email}</p>
+                        </div>
+                    );
                 })}
             </div>
         )
